222e1dd5ed085dc7f93906ebe9efe2e0d8dc8d97
jungsa <106296196+knkevol@users.noreply.github.com>
2025-09-30 10:05:28 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int Input = 0;
+	int Sum = 0;
+
+	cin >> Input;
+
+	
+	vector<int> XorNum(Input);
+
+	for (int i = 0; i < Input; i++)
+	{
+		int Num = 0;
+		cin >> Num;
+		if (!(cin >> Num))
+		{
+			return 1;
+		}			
+
+		int Check = 1;
+		while (Check <= Num)
+		{
+			if (Check > INT_MAX / 2)
+			{
+				Check = Num + 1;
+				break;
+			}
+			Check = Check * 2;
+		}
+
+		XorNum[i] = Check;
+	}
+
+
+	for (int k = 0; k < Input; k++)
+	{
+		Sum ^= XorNum[k];
+	}
+	
+	cout << Sum << endl;
+
+	
+	return 0;
+}
